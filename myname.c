64a9b48263dd945a9602217d4289df39655b49bf
Tushar Gaikwad <72184118+devSython@users.noreply.github.com>
2021-10-02 16:45:58 +0530

    Update myname.c

--- myname.c
+++ myname.c
@@ -2,5 +2,11 @@
 
 int main()
 {
-  printf("Mah name is);
+  printf("Mah name is");
+  return 0;
+  printf("This is my fiest program :) ");
+  
+  int n;
+  scanf("%d",&n);
+  int *stack = (int * ) malloc(n*sizeof(int));
 }
